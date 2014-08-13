6c5427feb166b217d5f316b00bdda90675787622
aromano272 <andre.romano272@gmail.com>
2014-08-13 05:50:56 +0100

    finishing up animations on menu icon, made menu icon unselectable

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
 
 
 var $menuOverlay = $("<div class='menu-overlay'></div>");
-var $menuIcon = $(".icon-menu");
+var $menuIcon = $(".menu-icon");
 var $menuBody = $(".main-nav");
 
 var menu_fadeOutTime = 200;
@@ -18,31 +18,37 @@ $menuIcon.click(function() {
 
   if($menuBody.hasClass("show-nav")) {
     // nav close
-    $menuBody.removeClass("show-nav");
-    $menuIcon.removeClass("show-nav");
-    // fade in content, shows menu icon after animation is finished
-    $menuOverlay.stop(true,false).fadeOut(menu_fadeOutTime, function() { 
-      $(".icon-menu span").show();
-    });
-    $(".icon-menu p").fadeOut(100);
+    hideMainNav();
   } else {
     // nav open
-    $menuBody.addClass("show-nav");
-    $menuIcon.addClass("show-nav");
-    $(".icon-menu span").hide();
-    // fade out content
-    $menuOverlay.stop(true,false).fadeIn(menu_fadeInTime, function() { 
-      $(".icon-menu p").fadeIn(100); 
-    });
+    showMainNav();
   }
 });
+
+var showMainNav = function() {
+  // nav open
+  $menuBody.addClass("show-nav");
+  $menuIcon.addClass("show-nav");
+  $(".menu-icon span").fadeOut(200);
+  // fade out content
+  $menuOverlay.stop(true,false).fadeIn(menu_fadeInTime);
+  $(".menu-icon p").fadeIn(500); 
+}
+
+var hideMainNav = function() {
+  // nav close
+  $menuBody.removeClass("show-nav");
+  $menuIcon.removeClass("show-nav");
+  // fade in content
+  $menuOverlay.stop(true,false).fadeOut(menu_fadeOutTime);
+  $(".menu-icon span").fadeIn(200);
+  $(".menu-icon p").fadeOut(200);
+}
 
 // fades in content on menuOverlay click
 $menuOverlay.click(function() {
   // nav close
-  $menuBody.removeClass("show-nav");
-  // fade in content
-  $menuOverlay.stop(true,false).fadeOut(menu_fadeOutTime);
+  hideMainNav();
 });
 
 
