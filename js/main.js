247f02e08264d5c8e2fea459ad581aa5eec1e486
aromano272 <andre.romano272@gmail.com>
2014-08-13 04:46:40 +0100

    trying to make menu icon stretch the size of menu nav, fade in and out 'close menu'

--- js/main.js
+++ js/main.js
@@ -19,13 +19,21 @@ $menuIcon.click(function() {
   if($menuBody.hasClass("show-nav")) {
     // nav close
     $menuBody.removeClass("show-nav");
-    // fade in content
-    $menuOverlay.stop(true,false).fadeOut(menu_fadeOutTime);
+    $menuIcon.removeClass("show-nav");
+    // fade in content, shows menu icon after animation is finished
+    $menuOverlay.stop(true,false).fadeOut(menu_fadeOutTime, function() { 
+      $(".icon-menu span").show();
+    });
+    $(".icon-menu p").fadeOut(100);
   } else {
     // nav open
     $menuBody.addClass("show-nav");
+    $menuIcon.addClass("show-nav");
+    $(".icon-menu span").hide();
     // fade out content
-    $menuOverlay.stop(true,false).fadeIn(menu_fadeInTime);
+    $menuOverlay.stop(true,false).fadeIn(menu_fadeInTime, function() { 
+      $(".icon-menu p").fadeIn(100); 
+    });
   }
 });
 
